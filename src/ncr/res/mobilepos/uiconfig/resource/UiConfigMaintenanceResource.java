dd15e57deb79af707939dfbd7dc52d2b8cd5dacb
gz250021 <gz250021@ncr.com>
2016-04-27 13:16:06 +0000

    RES-15196 by yhz
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/WIP@50477 9234da4d-b7fe-4b44-acf3-10f21a033f33

--- src/ncr/res/mobilepos/uiconfig/resource/UiConfigMaintenanceResource.java
+++ src/ncr/res/mobilepos/uiconfig/resource/UiConfigMaintenanceResource.java
@@ -10,18 +10,26 @@
 
 package ncr.res.mobilepos.uiconfig.resource;
 
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 import ncr.realgate.util.Trace;
+import ncr.res.mobilepos.exception.DaoException;
 import ncr.res.mobilepos.helper.DebugLogger;
 import ncr.res.mobilepos.helper.Logger;
+import ncr.res.mobilepos.helper.StringUtility;
+import ncr.res.mobilepos.model.ResultBase;
 import ncr.res.mobilepos.uiconfig.constants.UiConfigProperties;
+import ncr.res.mobilepos.uiconfig.dao.IUiConfigCommonDAO;
+import ncr.res.mobilepos.uiconfig.dao.SQLServerUiConfigCommonDAO;
+import ncr.res.mobilepos.uiconfig.model.schedule.CompanyInfoList;
 
 
 /**
- * Journalization Web Resource Class.
+ * uiconfigMaintenance Web Resource Class.
  *
- * <P>Journalize transaction.
+ * <P>uiconfigMaintenance.
  *
  */
 @Path("/uiconfigMaintenance")
@@ -52,6 +60,44 @@ public class UiConfigMaintenanceResource {
         this.tp = DebugLogger.getDbgPrinter(Thread.currentThread().getId(), getClass());
     }
 
-
+    @Path("/getcompanyinfo")
+    @POST
+    @Produces({"application/json;charset=UTF-8"})
+    
+	public final CompanyInfoList getCompanyInfo() {
+		// Logs given parameters.
+		String functionName = DebugLogger.getCurrentMethodName();
+		tp.methodEnter("/getcompanyinfo/");
+		
+		CompanyInfoList companyInfo = null;
+		try {
+			companyInfo = new CompanyInfoList();
+			IUiConfigCommonDAO icmyInfoDao = new SQLServerUiConfigCommonDAO();
+			if(StringUtility.isNullOrEmpty(icmyInfoDao.getCompanyInfo())) {
+				tp.println(ResultBase.RES_INVALIDPARAMETER_MSG);
+				companyInfo.setNCRWSSResultCode(ResultBase.RES_ERROR_INVALIDPARAMETER);
+				companyInfo.setNCRWSSExtendedResultCode(ResultBase.RES_ERROR_INVALIDPARAMETER);
+				companyInfo.setMessage(ResultBase.RES_INVALIDPARAMETER_MSG);
+                return companyInfo;
+			} else {
+				companyInfo.setCompanyInfo(icmyInfoDao.getCompanyInfo());
+			}
+		} catch (DaoException ex) {
+			tp.println("Failed to get company information.");
+			LOGGER.logAlert(functionName,
+					Logger.RES_EXCEP_DAO,
+					functionName + ":Failed to get company information.",
+					ex);
+		} catch(Exception ex) {
+			tp.println("Failed to get company information.");
+			LOGGER.logAlert(functionName,
+					Logger.RES_EXCEP_GENERAL,
+					functionName + ":Failed to get company information.",
+					ex);
+        } finally {
+        	tp.methodExit(companyInfo.toString());
+        }
+		return companyInfo;
+    }
 }
 

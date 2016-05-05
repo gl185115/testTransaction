4703d0583956116ef1071766204e8b85b508c747
gz250021 <gz250021@ncr.com>
2016-05-05 06:27:48 +0000

    RES-15196
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/WIP@50757 9234da4d-b7fe-4b44-acf3-10f21a033f33

--- WebContent/resUi/iframes/home/modules/menu/menu.js
+++ WebContent/resUi/iframes/home/modules/menu/menu.js
@@ -3,71 +3,89 @@
  */
 res.ui.controller("menu", ["$scope", "$rootScope", function($scope, $rootScope) {
 
-	$scope.popupMessage = "";
+    $scope.popupMessage = "";
 
-	$scope.page = function(choice) {
-		window.parent.res.main.page(choice);
+    $scope.page = function(choice) {
+        window.parent.res.main.page(choice);
 
-		switch (choice) {
-		case "noticesEditor":
-			res.ui.send({
-				context : "Notice",
-				event : "file.list",
-				data : {
-					folder : "notices"
-				}
-			});
-			break;
-		case "pickListEditor":
-			res.ui.send({
-				context : "PickList",
-				event : "file.list",
-				data : {
-					folder : "pickList"
-				}
-			});
-			break;
-		case "pickListAlbum":
-			res.ui.send({
-				context : "PickList.Album",
-				event : "file.picture.list",
-				data : {
-					folder : "images/pickList"
-				}
-			});
-			break;
-		case "noticesAlbum":
-			res.ui.send({
-				context : "Notices.Album",
-				event : "file.picture.list",
-				data : {
-					folder : "images/notices"
-				}
-			});
-			break;
-		default:
-			break;
-		}
+        switch (choice) {
+        case "noticesEditor":
+            res.ui.send({
+                context : "Notice",
+                event : "file.list",
+                data : {
+                    folder : "notices"
+                }
+            });
+            break;
+        case "pickListEditor":
+            res.ui.send({
+                context : "PickList",
+                event : "file.list",
+                data : {
+                    folder : "pickList"
+                }
+            });
+            break;
+        case "pickListAlbum":
+            res.ui.send({
+                context : "PickList.Album",
+                event : "file.picture.list",
+                data : {
+                    folder : "images/pickList"
+                }
+            });
+            break;
+        case "noticesAlbum":
+            res.ui.send({
+                context : "Notices.Album",
+                event : "file.picture.list",
+                data : {
+                    folder : "images/notices"
+                }
+            });
+            break;
+        case "advertisementEditor":
+            res.ui.send({
+                context : "Advertise",
+                event : "file.list",
+                data : {
+                    typeParam : "advertisement"
+                }
+            });
+            break;
+        case "advertisementAlbum":
+            res.ui.send({
+                context : "Advertise.Album",
+                event : "file.picture.list",
+                data : {
+                    folder : "images/advertisement"
+                }
+            });
+            break;
+        default:
+            break;
+        }
 
-	};
+    };
 
-	$scope.demoSetUp = function() {
-		// demo setup configuration
-		$.ajax({
-			type : 'POST',
-			url : res.config.baseURL + "demoSetup",
-			success : function() {
-				res.console("resUiConfig: successfully copied demo sample config files under /custom!");
-			},
-			error : function() {
-				res.console("resUiConfig: failed to copy demo sample config files under /custom!");
-			}
-		});
-	};
+    $scope.demoSetUp = function() {
+        // demo setup configuration
+        $.ajax({
+            type : 'POST',
+            url : res.config.baseURL + "demoSetup",
+            success : function() {
+                res.console("resUiConfig: successfully copied demo sample config files under /custom!");
+            },
+            error : function() {
+                res.console("resUiConfig: failed to copy demo sample config files under /custom!");
+            }
+        });
+    };
 
-	$scope.transfer = function(option) {
-		res.ui.root.PopupSetMessage = "PopupSetMessage";
-		res.ui.root.model.transferOption = option;
-	};
+    $scope.transfer = function(option) {
+        res.ui.root.PopupSetMessage = "PopupSetMessage";
+        res.ui.root.model.transferOption = option;
+    };
 
 }]);

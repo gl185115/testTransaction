00c725b2b9fd7ecb8e06a525cfb658571563f50f
gz250021 <gz250021@ncr.com>
2016-05-05 08:45:01 +0000

    RES-15196
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/WIP@50796 9234da4d-b7fe-4b44-acf3-10f21a033f33

--- WebContent/resUi/iframes/advertise/deploy/modules/deConfirm/deConfirm.js
+++ WebContent/resUi/iframes/advertise/deploy/modules/deConfirm/deConfirm.js
@@ -3,48 +3,48 @@
  */
 res.ui.controller("deConfirm", ["$scope", "$rootScope", "$timeout", "$filter", function ($scope, $rootScope, $timeout, $filter) {
 
-	$scope.deleteStoreOkay = function () {
-		var entry = angular.element(document.getElementById("EditStore")).scope();
-		$rootScope.secondPhrase = "DeleteStore.";
+    $scope.deleteStoreOkay = function () {
+        var entry = angular.element(document.getElementById("EditStore")).scope();
+        $rootScope.secondPhrase = "DeleteStore.";
 
-		var pStoreId = entry.editStoreId.trim();
-		var pStoreNameJ = entry.editStoreNmJp.trim();
-		var pStorePriIp = entry.editStoreIP == "" ? " " : entry.editStoreIP.trim();
-		var pStoreSecIp = entry.editStoreSecondaryIP == "" ? " " : entry.editStoreSecondaryIP.trim();
-		var pStorePhone = entry.editStoreTel == "" ? " " : entry.editStoreTel.trim();
+        var pStoreId = entry.editStoreId.trim();
+        var pStoreNameJ = entry.editStoreNmJp.trim();
+        var pStorePriIp = entry.editStoreIP == "" ? " " : entry.editStoreIP.trim();
+        var pStoreSecIp = entry.editStoreSecondaryIP == "" ? " " : entry.editStoreSecondaryIP.trim();
+        var pStorePhone = entry.editStoreTel == "" ? " " : entry.editStoreTel.trim();
 
-		res.ui.send({
-			context : res.ui.context,
-			event : "deploy.pickList.editTableStore",
-			data : {
-				editType: "delete" ,
-				storeInfo: pStoreId + "," + pStoreNameJ + "," + pStorePriIp + "," + pStoreSecIp + "," + pStorePhone
-			}
-		});
+        res.ui.send({
+            context : res.ui.context,
+            event : "deploy.advertise.editTableStore",
+            data : {
+                editType: "delete" ,
+                storeInfo: pStoreId + "," + pStoreNameJ + "," + pStorePriIp + "," + pStoreSecIp + "," + pStorePhone
+            }
+        });
 
-		$rootScope.deConfirm = "";
-		$rootScope.dialog = "";
-		$rootScope.model.pageIndex = "";
-	};
+        $rootScope.deConfirm = "";
+        $rootScope.dialog = "";
+        $rootScope.model.pageIndex = "";
+    };
 
-//	$scope.deleteStoreOkay = function () {
-//		var obj = angular.element(document.getElementById("EditStore")).scope();
-//		$rootScope.secondPhrase = "DeleteStore.";
-//		obj.storeLine.commandType = "D";
-//		res.ui.send({event:"deploy.store.editStore.deleOkay",
-//			data:{
-//				'lineType': obj.storeLine.lineType,
-//				'commandType': obj.storeLine.commandType ,
-//				'line': obj.editStoreNmEn + "," + obj.editStoreNmJp + "," + obj.editStoreId+","+ obj.editStoreIP,
-//				'lineDetail' : obj.editStoreId+","+ obj.editStoreSecondaryIP
-//			}});
-//		$rootScope.deConfirm = "";
-//		$rootScope.dialog = "";
-//		$rootScope.model.pageIndex = "";
+//  $scope.deleteStoreOkay = function () {
+//      var obj = angular.element(document.getElementById("EditStore")).scope();
+//      $rootScope.secondPhrase = "DeleteStore.";
+//      obj.storeLine.commandType = "D";
+//      res.ui.send({event:"deploy.store.editStore.deleOkay",
+//          data:{
+//              'lineType': obj.storeLine.lineType,
+//              'commandType': obj.storeLine.commandType ,
+//              'line': obj.editStoreNmEn + "," + obj.editStoreNmJp + "," + obj.editStoreId+","+ obj.editStoreIP,
+//              'lineDetail' : obj.editStoreId+","+ obj.editStoreSecondaryIP
+//          }});
+//      $rootScope.deConfirm = "";
+//      $rootScope.dialog = "";
+//      $rootScope.model.pageIndex = "";
 //    };
 
-	$scope.voidDelete = function () {
-		$rootScope.deConfirm = "";
-		$rootScope.dialog = "editStore";
+    $scope.voidDelete = function () {
+        $rootScope.deConfirm = "";
+        $rootScope.dialog = "editStore";
     };
 }]);

4d5fe1c99105b6d7602dcd9b058557c71937e312
gz250021 <gz250021@ncr.com>
2016-05-05 06:28:25 +0000

    RES-15196
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/WIP@50759 9234da4d-b7fe-4b44-acf3-10f21a033f33

--- WebContent/resUi/iframes/home/res.model.js
+++ WebContent/resUi/iframes/home/res.model.js
@@ -2,44 +2,46 @@ var res = res || {};
 res.model = res.model || {};
 
 /*
- *	Data Model for Home Page
+ *  Data Model for Home Page
  */
 (function(){
 
-	res.model.init = function() {
-		res.ui.root.model = new Home();
-	};
+    res.model.init = function() {
+        res.ui.root.model = new Home();
+    };
 
-	res.model.update = function(event, data) {
-		var model = res.ui.root.model;
-		switch(event){
-		case "deploy.pickList.invokeTransfer.successful":
-		case "deploy.pickList.invokeTransfer.failed":
-		case "deploy.notices.invokeTransfer.successful":
-		case "deploy.notices.invokeTransfer.failed":
-			model.resultCode = data.status;
-			switch(model.resultCode){
-			case "success":
-				model.transfering = false;
-				window.parent.res.ui.root.model.popup = "";
-				if (!window.parent.res.ui.root.$$phase) window.parent.res.ui.root.$digest();
-				//alert("Transfer invoke successfully");
-				break;
-			case "failed":
-			case "error":
-				model.transfering = false;
-				window.parent.res.ui.root.model.popup = "";
-				if (!window.parent.res.ui.root.$$phase) window.parent.res.ui.root.$digest();
-				//alert("Transfer invoke failed");
-				break;
-			}
+    res.model.update = function(event, data) {
+        var model = res.ui.root.model;
+        switch(event){
+        case "deploy.advertise.invokeTransfer.successful":
+        case "deploy.advertise.invokeTransfer.failed":
+        case "deploy.pickList.invokeTransfer.successful":
+        case "deploy.pickList.invokeTransfer.failed":
+        case "deploy.notices.invokeTransfer.successful":
+        case "deploy.notices.invokeTransfer.failed":
+            model.resultCode = data.status;
+            switch(model.resultCode){
+            case "success":
+                model.transfering = false;
+                window.parent.res.ui.root.model.popup = "";
+                if (!window.parent.res.ui.root.$$phase) window.parent.res.ui.root.$digest();
+                //alert("Transfer invoke successfully");
+                break;
+            case "failed":
+            case "error":
+                model.transfering = false;
+                window.parent.res.ui.root.model.popup = "";
+                if (!window.parent.res.ui.root.$$phase) window.parent.res.ui.root.$digest();
+                //alert("Transfer invoke failed");
+                break;
+            }
 
-			break;
-		default:
-			alert("home: unknown message.event = " + event);
-			break;
-		}
-	};
+            break;
+        default:
+            alert("home: unknown message.event = " + event);
+            break;
+        }
+    };
 
 })();
 
@@ -48,12 +50,12 @@ res.model = res.model || {};
  */
 (function(){
 
-	Home = function() {
-		this.version = "UC " + res.config.version;
-		this.applicationName = "かんたんPOS";
-		this.transferOption = undefined;
-	};
+    Home = function() {
+        this.version = "UC " + res.config.version;
+        this.applicationName = "かんたんPOS";
+        this.transferOption = undefined;
+    };
 
-	Home.prototype = {};
+    Home.prototype = {};
 
 })();

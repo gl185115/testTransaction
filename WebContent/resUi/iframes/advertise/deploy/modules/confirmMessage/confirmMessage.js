2f7ea6c77eaebab101d9599a06c5b71de98a7285
gz250021 <gz250021@ncr.com>
2016-05-05 08:43:06 +0000

    RES-15196
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/WIP@50795 9234da4d-b7fe-4b44-acf3-10f21a033f33

--- WebContent/resUi/iframes/advertise/deploy/modules/confirmMessage/confirmMessage.js
+++ WebContent/resUi/iframes/advertise/deploy/modules/confirmMessage/confirmMessage.js
@@ -3,28 +3,28 @@
  */
 res.ui.controller("confirmMessage", ["$scope", "$rootScope", "$timeout", "$filter", function ($scope, $rootScope, $timeout, $filter) {
 
-	$scope.Okay = function() {
-		$rootScope.model.save();
-		$rootScope.model.active.item = undefined;
-		$rootScope.model.message = "SaveInvoke";
-		var jsonSchedule = JSON.stringify({ schedule : $rootScope.model.schedule });
+    $scope.Okay = function() {
+        $rootScope.model.save();
+        $rootScope.model.active.item = undefined;
+        $rootScope.model.message = "SaveInvoke";
+        var jsonSchedule = JSON.stringify({ schedule : $rootScope.model.schedule });
 
-		res.console("res.ui deploy menu save: model.schedule = " + jsonSchedule);
+        res.console("res.ui deploy menu save: model.schedule = " + jsonSchedule);
 
-		res.ui.send({
-			context : res.ui.context,
-			event : "deploy.pickList.setSchedule",
-			data : {
-				schedule : jsonSchedule,
-				resource : "pickList"
-			}
-		});
+        res.ui.send({
+            context : res.ui.context,
+            event : "deploy.advertise.setSchedule",
+            data : {
+                schedule : jsonSchedule,
+                resource : "advertise"
+            }
+        });
 
-		$rootScope.confirmMessage = "";
-	};
+        $rootScope.confirmMessage = "";
+    };
 
-	$scope.voidSelect = function() {
-		$rootScope.confirmMessage = "";
-	};
+    $scope.voidSelect = function() {
+        $rootScope.confirmMessage = "";
+    };
 
 }]);

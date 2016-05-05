ada6d8ca0caf129f549ef3755ff6f23234df83a4
gz250021 <gz250021@ncr.com>
2016-05-05 06:28:05 +0000

    RES-15196
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/WIP@50758 9234da4d-b7fe-4b44-acf3-10f21a033f33

--- WebContent/resUi/iframes/home/modules/popupMessage/popupMessage.js
+++ WebContent/resUi/iframes/home/modules/popupMessage/popupMessage.js
@@ -3,32 +3,34 @@
  */
 res.ui.controller("PopupMessage", ["$scope", "$rootScope", "$timeout", "$filter", function ($scope, $rootScope, $timeout, $filter) {
 
-	$scope.okaySet = function(selectFlag) {
-		if (!$rootScope.model.transfering) {
-			window.parent.res.ui.root.model.popup = "Wait";
-			if (!window.parent.res.ui.root.$$phase) window.parent.res.ui.root.$digest();
-			$rootScope.model.transfering = true;
+    $scope.okaySet = function(selectFlag) {
+        if (!$rootScope.model.transfering) {
+            window.parent.res.ui.root.model.popup = "Wait";
+            if (!window.parent.res.ui.root.$$phase) window.parent.res.ui.root.$digest();
+            $rootScope.model.transfering = true;
 
-			if ($rootScope.model.transferOption == "notices") {
-				var msg = "deploy.notices.invokeTransfer";
-			} else if ($rootScope.model.transferOption == "pickList") {
-				var msg = "deploy.pickList.invokeTransfer";
-			}
+            if ($rootScope.model.transferOption == "notices") {
+                var msg = "deploy.notices.invokeTransfer";
+            } else if ($rootScope.model.transferOption == "pickList") {
+                var msg = "deploy.pickList.invokeTransfer";
+            }else if ($rootScope.model.transferOption == "advertise") {
+                var msg = "deploy.advertise.invokeTransfer";
+            }
 
-			res.ui.send({
-				context : res.ui.context,
-				event : msg,
-				data : {
-					transferOption : $rootScope.model.transferOption
-				}
-			});
-		}
+            res.ui.send({
+                context : res.ui.context,
+                event : msg,
+                data : {
+                    transferOption : $rootScope.model.transferOption
+                }
+            });
+        }
 
-		$rootScope.PopupSetMessage = "";
-	};
+        $rootScope.PopupSetMessage = "";
+    };
 
-	$scope.voidSet = function() {
-		$rootScope.PopupSetMessage = "";
-	};
+    $scope.voidSet = function() {
+        $rootScope.PopupSetMessage = "";
+    };
 
 }]);

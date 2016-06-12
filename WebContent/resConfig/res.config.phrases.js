10d482817c1c22857d95d3630e6e8a71bccfc372
gz250021 <gz250021@ncr.com>
2016-06-12 09:41:13 +0000

    RES-15196 by sbq
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/WIP@53999 9234da4d-b7fe-4b44-acf3-10f21a033f33

--- WebContent/resConfig/res.config.phrases.js
+++ WebContent/resConfig/res.config.phrases.js
@@ -1038,7 +1038,7 @@ res.config.phrases = {
 //    "Advertise.Failure.advertise.yearError":["The beginning of the year is bigger than the end of year","開始年 > 終了年 ,正しく入力して下さい!"],
 //    "Advertise.Failure.advertise.monthError":["The beginning of the month is bigger than the end of month","開始月 > 終了月 ,正しく入力して下さい!"],
     "Advertise.Failure.advertise.dateError":["The beginning of the date is bigger than the end of date","開始日付 > 終了日付 ,正しく入力して下さい!"],
-    "Advertise.Failure.advertise.timeError":["Please enter 1-60 seconds","スライド間隔に1-60の整数を入力してください"],
+    "Advertise.Failure.advertise.timeError":["Invalid value for the Time Interval, It should be between 1 and 60.","スライド間隔の設定値が不正です、１～６０の範囲で設定してください。"],
     "Advertise.Failure.advertise.InputError":["Please enter numbers","数字を入力してください"],
     "Advertise.Failure.advertise.maxNumbers13":["Can not input more than 13 numbers!","商品番号には、最大13桁の数字(半角)を入力してください。"],
     "Advertise.Failure.advertise.maxCharacters6":["Can not input more than 6 characters!","商品名には、全角6文字まで入力してください。"],

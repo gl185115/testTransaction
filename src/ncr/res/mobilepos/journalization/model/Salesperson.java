cb045078e5a8d8c3f47d4b22a62dccbcdbbf72d6
gz250021 <gz250021@ncr.com>
2016-06-16 01:30:31 +0000

    RES-16004 SMY
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/WIP@54439 9234da4d-b7fe-4b44-acf3-10f21a033f33

--- src/ncr/res/mobilepos/journalization/model/Salesperson.java
+++ src/ncr/res/mobilepos/journalization/model/Salesperson.java
@@ -8,6 +8,10 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+
+import com.wordnik.swagger.annotations.ApiModel;
+import com.wordnik.swagger.annotations.ApiModelProperty;
+
 import ncr.res.mobilepos.model.ResultBase;
 /**
  * @author 
@@ -16,6 +20,7 @@ import ncr.res.mobilepos.model.ResultBase;
  */
 @XmlRootElement(name = "Salesperson")
 @XmlAccessorType(XmlAccessType.NONE)
+@ApiModel(value="Salesperson")
 public class Salesperson extends ResultBase{
 	
 	/*This list is used to save sales person information*/
@@ -27,6 +32,7 @@ public class Salesperson extends ResultBase{
 	 *
 	 * @return the sales person list
 	 */
+    @ApiModelProperty(value="販売員コード", notes="販売員コード")
     public final Salespersoninfo[] getSalesperson() {
         return Salesperson;
     }

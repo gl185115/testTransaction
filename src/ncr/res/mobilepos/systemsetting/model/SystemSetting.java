ba7eecd43ea83a86890d4e0201a35906787acd1c
gz250021 <gz250021@ncr.com>
2016-06-17 06:42:32 +0000

    RES-16004 ZHF
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/WIP@54619 9234da4d-b7fe-4b44-acf3-10f21a033f33

--- src/ncr/res/mobilepos/systemsetting/model/SystemSetting.java
+++ src/ncr/res/mobilepos/systemsetting/model/SystemSetting.java
@@ -5,6 +5,9 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import com.wordnik.swagger.annotations.ApiModel;
+import com.wordnik.swagger.annotations.ApiModelProperty;
+
 import ncr.res.mobilepos.model.ResultBase;
 
 /**
@@ -13,6 +16,7 @@ import ncr.res.mobilepos.model.ResultBase;
  */
 @XmlAccessorType(XmlAccessType.NONE)
 @XmlRootElement(name = "SystemSetting")
+@ApiModel(value="SystemSetting")
 public class SystemSetting extends ResultBase {
     /**
      * Date Setting.
@@ -24,6 +28,7 @@ public class SystemSetting extends ResultBase {
      * Getter for Date Setting.
      * @return  date setting
      */
+    @ApiModelProperty( value="ì˙ïtê›íË", notes="ì˙ïtê›íË")
     public final DateSetting getDateSetting() {
         return dateSetting;
     }

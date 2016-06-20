45d254bfa4a672a0df15e9e99a18cadb983891e6
gz250021 <gz250021@ncr.com>
2016-06-20 03:29:15 +0000

    RES-16004 ZHF
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/WIP@54764 9234da4d-b7fe-4b44-acf3-10f21a033f33

--- src/ncr/res/mobilepos/softwareinfo/model/OperatingSystemInfo.java
+++ src/ncr/res/mobilepos/softwareinfo/model/OperatingSystemInfo.java
@@ -14,12 +14,15 @@ import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
 
+import com.wordnik.swagger.annotations.ApiModel;
+
 /**
  * OperatingSystemInfo Model class for application's software vesion.
  *
  */
 @XmlAccessorType(XmlAccessType.NONE)
 @XmlRootElement(name = "OperatingSystemInfo")
+@ApiModel(value="OperatingSystemInfo")
 public class OperatingSystemInfo extends SoftwareVersion  {
 
     /**

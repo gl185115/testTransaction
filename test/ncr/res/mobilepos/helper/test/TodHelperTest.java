28e4caf8facb9ace0edbba027baf21bfe47ad36e
ts185174 <ts185174@ncr.com>
2016-08-18 03:49:25 +0000

    MEXSVR-16 Deleted and cleaned up out-dated test cases.
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/3.02_Q3_RES16511@58637 9234da4d-b7fe-4b44-acf3-10f21a033f33

--- test/ncr/res/mobilepos/helper/test/TodHelperTest.java
+++
@@ -1,9 +0,0 @@
-package ncr.res.mobilepos.helper.test;
-
-import ncr.res.mobilepos.test.TestRunnerScenario;
-
-public class TodHelperTest extends TestRunnerScenario {
-    public TodHelperTest(){
-        super(new TodHelperSteps());
-    }
-}

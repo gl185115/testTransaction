2891ac45527e1dbdb72796a1843a981429132f31
zu185002 <zu185002@ncr.com>
2017-01-03 10:51:09 +0000

    MEXSVR-109 - Add component test (Successful Registration - Upon Device Registration)
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/3.02_Q4@63948 9234da4d-b7fe-4b44-acf3-10f21a033f33

--- test/ncr/res/mobilepos/authentication/test/RegistrationSteps.java
+++ test/ncr/res/mobilepos/authentication/test/RegistrationSteps.java
@@ -1,12 +1,14 @@
 package ncr.res.mobilepos.authentication.test;
 
 import java.lang.reflect.Field;
+import java.util.Map;
 
 import javax.servlet.ServletContext;
 
 import junit.framework.Assert;
 import ncr.res.mobilepos.authentication.model.DeviceStatus;
 import ncr.res.mobilepos.authentication.resource.AuthenticationResource;
+import ncr.res.mobilepos.authentication.resource.RegistrationResource;
 import ncr.res.mobilepos.deviceinfo.model.DeviceAttribute;
 import ncr.res.mobilepos.deviceinfo.model.ViewDeviceInfo;
 import ncr.res.mobilepos.deviceinfo.resource.DeviceInfoResource;
@@ -21,6 +23,7 @@ import org.jbehave.scenario.annotations.BeforeScenario;
 import org.jbehave.scenario.annotations.Given;
 import org.jbehave.scenario.annotations.Then;
 import org.jbehave.scenario.annotations.When;
+import org.jbehave.scenario.definition.ExamplesTable;
 import org.jbehave.scenario.steps.Steps;
 
 public class RegistrationSteps extends Steps {
@@ -37,7 +40,8 @@ public class RegistrationSteps extends Steps {
 	private DeviceAttribute deviceAttribute = null;
 	private DeviceStatus deviceStatus = null;
 	private ResultBase resultbase = null;
-
+	private RegistrationResource registerResource = null;
+	
 	@BeforeScenario
 	public final void SetUpClass() {
 		dbInit = new DBInitiator("SQLServerItemDAOSteps", DATABASE.RESMaster);
@@ -67,6 +71,7 @@ public class RegistrationSteps extends Steps {
 			servletContext = Requirements.getMockServletContext();
 			deviceInfoResource = new DeviceInfoResource();
 			authenticationResource = new AuthenticationResource();
+			registerResource = new RegistrationResource();
 			Field deviceContext;
 			try {
 				deviceContext = deviceInfoResource.getClass().getDeclaredField(
@@ -78,6 +83,11 @@ public class RegistrationSteps extends Steps {
 						.getDeclaredField("context");
 				deviceContext.setAccessible(true);
 				deviceContext.set(authenticationResource, servletContext);
+				
+				deviceContext = registerResource.getClass()
+						.getDeclaredField("context");
+				deviceContext.setAccessible(true);
+				deviceContext.set(registerResource, servletContext);
 
 				dbInit.ExecuteOperation(DatabaseOperation.CLEAN_INSERT,
 						systemConDataSet);
@@ -89,7 +99,7 @@ public class RegistrationSteps extends Steps {
 		}
 	}
 	
-	@When("register companyid{$companyid} storeid{$storeid} terminalid{$terminalid}")
+	@When("I get device attribute companyid{$companyid} storeid{$storeid} terminalid{$terminalid}")
 	public final void whenIRegisterDevice(String companyId, String storeId, String terminalId){
 		deviceAttribute = deviceInfoResource.getDeviceAttribute(companyId, storeId, terminalId);
 	}
@@ -99,6 +109,38 @@ public class RegistrationSteps extends Steps {
 		int result = Integer.parseInt(resultCode);
 		Assert.assertEquals(result, deviceAttribute.getNCRWSSResultCode());
 	}
+	
+	@Then("I should get the following: $data")
+	public final void thenIShouldGetData(ExamplesTable expectedData) {
+		for (Map<String, String> data : expectedData.getRows()) {
+			Assert.assertEquals(data.get("Message").trim(), deviceAttribute.getMessage());
+			Assert.assertEquals(data.get("CompanyId").trim(), deviceAttribute.getCompanyId());
+			Assert.assertEquals(data.get("StoreId").trim(), deviceAttribute.getStoreId());
+			Assert.assertEquals(data.get("TerminalId").trim(), deviceAttribute.getTerminalId());
+			Assert.assertEquals(data.get("DeviceName").trim(), deviceAttribute.getDeviceName());
+			Assert.assertEquals(data.get("NCRWSSResultCode").trim(), String.valueOf(deviceAttribute.getNCRWSSResultCode()));
+		
+		}
+	}
+	
+	@When("I register device companyid{$companyid} storeid{$storeid} terminalid{$terminalid} devicename{$devicename} passcode{$passcode} uuid{$uuid} udid{$udid} signstatus{$signstatus}")
+	public final void whenIRegisterDevice(String companyId, String storeId, String terminalId, String deviceName, String passCode, String uuid, String udid, String signstatus){
+		int signin = Integer.parseInt(signstatus);
+		deviceStatus = registerResource.registerDevice(companyId, storeId, terminalId, deviceName, passCode, udid, uuid, signin, "", "");
+	}
+	
+	@Then("I should get the following registration response: $data")
+	public final void thenIShouldGetResponse(ExamplesTable expectedData) {
+		for (Map<String, String> data : expectedData.getRows()) {
+			Assert.assertEquals(data.get("Message").trim(), deviceStatus.getMessage());
+			Assert.assertEquals(data.get("StoreID").trim(), deviceStatus.getStoreID());
+			Assert.assertEquals(data.get("CorpID").trim(), deviceStatus.getCorpID());
+			Assert.assertEquals(data.get("TerminalId").trim(), deviceStatus.getTerminalID());
+			Assert.assertEquals(data.get("NCRWSSResultCode").trim(), String.valueOf(deviceStatus.getNCRWSSResultCode()));
+		
+		}
+	}
+	
 	
 	
 }

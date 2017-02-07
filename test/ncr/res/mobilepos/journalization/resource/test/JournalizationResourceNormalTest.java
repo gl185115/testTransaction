1899f4d2da0b5619f437688c88ad0b82feb8eff9
jd185128 <jd185128@ncr.com>
2017-02-07 09:34:47 +0000

    MEXSVR-141 -Revert saveposlog for Sales
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/3.02_Q4@65082 9234da4d-b7fe-4b44-acf3-10f21a033f33

---
+++ test/ncr/res/mobilepos/journalization/resource/test/JournalizationResourceNormalTest.java
@@ -0,0 +1,9 @@
+package ncr.res.mobilepos.journalization.resource.test;
+
+import ncr.res.mobilepos.test.TestRunnerScenario;
+
+public class JournalizationResourceNormalTest extends TestRunnerScenario {
+    public JournalizationResourceNormalTest(){
+        super(new JournalizationResourceSteps());
+    }
+}

d323cc63c414f745a4ec496775a909164e210c49
jd185128 <jd185128@ncr.com>
2017-03-07 06:32:53 +0000

    MEXSVR-163 - SoftwareInfo
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/3.3.0@65721 9234da4d-b7fe-4b44-acf3-10f21a033f33

--- test/ncr/res/mobilepos/softwareinfo/resource/test/SoftwareInfoSteps.java
+++ test/ncr/res/mobilepos/softwareinfo/resource/test/SoftwareInfoSteps.java
@@ -1,12 +1,9 @@
 package ncr.res.mobilepos.softwareinfo.resource.test;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
-
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.lang.reflect.Field;
+import java.util.Map;
 
 import javax.servlet.ServletContext;
 
@@ -17,6 +14,7 @@ import org.jbehave.core.annotations.BeforeScenario;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
+import org.jbehave.core.model.ExamplesTable;
 import org.jbehave.core.steps.Steps;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -27,12 +25,10 @@ import org.springframework.core.io.FileSystemResourceLoader;
 import ncr.res.mobilepos.helper.Requirements;
 import ncr.res.mobilepos.softwareinfo.resource.SoftwareInfo;
 import ncr.res.mobilepos.softwareinfo.model.SoftwareComponents;
-import ncr.res.mobilepos.softwareinfo.model.SoftwareVersion;
 
 public class SoftwareInfoSteps extends Steps {
 	@InjectMocks
     private SoftwareInfo service;
-    private SoftwareVersion software;
     private SoftwareComponents softwareComponents;
     @Mock
     private ServletContext context;
@@ -73,63 +69,7 @@ public class SoftwareInfoSteps extends Steps {
     	System.setProperty("os.version", "Actual");
     	System.setProperty("os.name", "Windows 7");
     	System.setProperty("os.arch", "amd64");
-    }
-    
-    @When("I get $software Information")
-    public final void getSoftwareInfo(final String softwareName){
-        if (softwareName.equals("Tomcat")) {
-        	stubTomcat();
-            software = service.getTomCatVersion();
-        } else if (softwareName.equals("J2EE")) {
-            software = service.getJavaVersion();
-        } else if (softwareName.equals("WebStoreServer")) {
-            software = service.getWebAPIVersion();
-        } else if (softwareName.equals("OperatingSystem")) {
-            software = service.getOperatingSystemInfo();    
-        } else {
-            software = new SoftwareVersion();
-        }
-    }    
-    
-    @When("I get all Software Information")
-    public final void getAllSoftwareInfo(){       
-    	softwareComponents = service.getAllSoftwareInfo();
-    }  
-    
-    @Then("I should get all other Software Information")
-    public final void shouldGetAllSoftwareInfo(){      	
-    	Assert.assertNotNull(softwareComponents); 
-    	
-    	software = softwareComponents.getServiceVersionInfo();
-    	this.shouldGetSoftwareInfo("WebStoreServer3", "s30wip");
-    	
-    	software = softwareComponents.getServerVersionInfo();
-    	this.shouldGetSoftwareInfo("Tomcat", "Actual");
-    	
-    	software = softwareComponents.getJavaVersionInfo();
-    	this.shouldGetSoftwareInfo("J2EE", "Actual");
-    	
-    	software = softwareComponents.getOperatingSystemVersionInfo();
-    	this.shouldGetSoftwareInfo("OperatingSystem", "Windows 7(amd64)Actual");
-    	
-    }
-    
-    @When("I get the Service Information")
-    public final void shouldGetServiceInfo(){      	
-    	Assert.assertNotNull(softwareComponents);    	
-    	software = softwareComponents.getServiceVersionInfo(); 
-    }    
-    
-    
-    
-    @Then("I should get $expected software version information")
-    public final void getNAWSSInfo(String expectedValue){
-        Assert.assertNotNull(software);
-        Assert.assertEquals(0, software.getNCRWSSResultCode());
-        Assert.assertTrue(null != software.getName() && !software.getName().isEmpty());
-        Assert.assertTrue(null != software.getVersion() && !software.getVersion().isEmpty());
-        Assert.assertEquals(expectedValue, software.getVersion());
-        Assert.assertEquals(expectedValue, "NA");
+    	System.setProperty("java.version", "Actual");
     }
     
     @Given("that the MANIFEST.MF file is not available in the META-INF")
@@ -139,38 +79,47 @@ public class SoftwareInfoSteps extends Steps {
 			Mockito.stub(context.getResourceAsStream("/META-INF/MANIFEST.MF")).toReturn(null);
 			Mockito.stub(context.getServerInfo()).toReturn("Actual");
 		} catch (Exception e) {}
-        
+    	System.setProperty("os.version", "Actual");
+    	System.setProperty("os.name", "Windows 7");
+    	System.setProperty("os.arch", "amd64");
+    	System.setProperty("java.version", "Actual");
     }
    
+    @When("I get all Software Information")
+    public final void getAllSoftwareInfo(){       
+    	softwareComponents = service.getAllSoftwareInfo();
+    }  
     
-    
-    private void stubTomcat(){
-    	MockitoAnnotations.initMocks(this);
-    	try {
-			Mockito.stub(context.getServerInfo()).toReturn("Actual");
-		} catch (Exception e) {}
-    }
-    
-    @Then("I should get Software Information with name {$name}"
-            + " and Version {$version}")
-	public final void shouldGetSoftwareInfo(final String name, String version) {
-    	try {
-			assertThat(name, is(equalTo(software.getName())));
-			if ((name.equals("J2EE")) && (version.equals("Actual"))) {
-				version = System.getProperty("java.version");
-			} else if ((name.equals("OperatingSystem"))
-					&& (version.equals("Actual"))) {			
-				version = System.getProperty("os.name");
+    @Then("I should get the following software version: $expected")
+    public final void shouldGetAllSoftwareInfo(ExamplesTable expected){      	
+    	Assert.assertNotNull(softwareComponents); 
+		for (Map<String, String> data : expected.getRows()) {
+			if (data.get("software").equalsIgnoreCase("java")) {
+				Assert.assertEquals("Compare java name", data.get("name"),
+						softwareComponents.getJavaVersionInfo().getName());
+				Assert.assertEquals("Compare java version",
+						data.get("version"), softwareComponents
+								.getJavaVersionInfo().getVersion());
+			} else if (data.get("software").equalsIgnoreCase("service")) {
+				Assert.assertEquals("Compare service name", data.get("name"),
+						softwareComponents.getServiceVersionInfo().getName());
+				Assert.assertEquals("Compare service version", data
+						.get("version"), softwareComponents
+						.getServiceVersionInfo().getVersion());
+			} else if (data.get("software").equalsIgnoreCase("server")) {
+				Assert.assertEquals("Compare server name", data.get("name"),
+						softwareComponents.getServerVersionInfo().getName());
+				Assert.assertEquals("Compare server version", data
+						.get("version"), softwareComponents
+						.getServerVersionInfo().getVersion());
+			} else if (data.get("software").equalsIgnoreCase("os")) {
+				Assert.assertEquals("Compare os name", data.get("name"),
+						softwareComponents.getOperatingSystemVersionInfo()
+								.getName());
+				Assert.assertEquals("Compare os version", data.get("version"),
+						softwareComponents.getOperatingSystemVersionInfo()
+								.getVersion());
 			}
-			assertThat(version, is(equalTo(software.getVersion())));
-    	} catch (Exception e) {}	
-	}
-    
-    @Then("I should get the software version information")
-    public final void testWSSInformation(){
-        Assert.assertNotNull(software);
-        Assert.assertEquals(0, software.getNCRWSSResultCode());
-        Assert.assertTrue(null != software.getName() && !software.getName().isEmpty());
-        Assert.assertTrue(null != software.getVersion() && !software.getVersion().isEmpty());
+		}
     }
 }

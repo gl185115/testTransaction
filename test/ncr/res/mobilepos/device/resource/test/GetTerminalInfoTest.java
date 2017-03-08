0908e9472dd984b110845dafe5c7e5ad595361a6
az250108 <az250108@ncr.com>
2017-03-08 06:14:59 +0000

    MEXSVR-153 -Revert deleted DeviceInfoResource test
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/3.3.0@65769 9234da4d-b7fe-4b44-acf3-10f21a033f33

---
+++ test/ncr/res/mobilepos/device/resource/test/GetTerminalInfoTest.java
@@ -0,0 +1,9 @@
+package ncr.res.mobilepos.device.resource.test;
+
+import ncr.res.mobilepos.test.TestRunnerScenario;
+
+public class GetTerminalInfoTest extends TestRunnerScenario {
+	public GetTerminalInfoTest() {
+		super(new GetTerminalInfoSteps());
+	}
+}

d263e6432b5dcc8e72fa596ea77368b92d2bfaf2
jb250646 <jb250646@ncr.com>
2017-03-15 03:46:37 +0000

    MEXSVR-153-Revert deleted DeviceInfoResource test
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/3.3.0@66014 9234da4d-b7fe-4b44-acf3-10f21a033f33

---
+++ test/ncr/res/mobilepos/attribute/test/GetAttributeTest.java
@@ -0,0 +1,11 @@
+package ncr.res.mobilepos.attribute.test;
+
+
+import ncr.res.mobilepos.test.TestRunnerScenario;
+
+public class GetAttributeTest extends TestRunnerScenario{
+    public GetAttributeTest(){
+        super(new GetAttributeSteps());
+
+}
+}

a5be5db890f863b67e73379c33f14931c970b49d
jd185128 <jd185128@ncr.com>
2017-03-30 04:07:31 +0000

    MEXSVR-199 - EOD Credit Screen without Credit transactiotn wihin the business day
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/3.3.0@66428 9234da4d-b7fe-4b44-acf3-10f21a033f33

---
+++ test/ncr/res/mobilepos/eod/test/EodCreditentryTest.java
@@ -0,0 +1,11 @@
+package ncr.res.mobilepos.eod.test;
+
+import ncr.res.mobilepos.test.TestRunnerScenario;
+
+public class EodCreditentryTest extends TestRunnerScenario {
+
+	public EodCreditentryTest() {
+		super(new EndOfDaySteps());
+	}
+
+}

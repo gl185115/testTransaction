a84ff1d0b0e9c91648005f716ccac5afc07a3faf
jd185128 <jd185128@ncr.com>
2017-03-30 04:05:59 +0000

    MEXSVR-201 -EOD non-cash summary report Screen without non-cash transaction within the business day
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/3.3.0@66427 9234da4d-b7fe-4b44-acf3-10f21a033f33

---
+++ test/ncr/res/mobilepos/eod/test/EodNoncashsummaryTest.java
@@ -0,0 +1,11 @@
+package ncr.res.mobilepos.eod.test;
+
+import ncr.res.mobilepos.test.TestRunnerScenario;
+
+public class EodNoncashsummaryTest extends TestRunnerScenario {
+
+	public EodNoncashsummaryTest() {
+		super(new EndOfDaySteps());
+	}
+
+}

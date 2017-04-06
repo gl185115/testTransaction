85c0b38cf55da335aedafc982b1011f6b97f0fe6
gz250021 <gz250021@ncr.com>
2017-04-06 11:35:33 +0000

    MEXSVR-176：giftCardサービスのJbehaveテストコードを追加すること。
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/3.3.0@66623 9234da4d-b7fe-4b44-acf3-10f21a033f33

---
+++ test/ncr/res/mobilepos/giftcard/test/GiftCardTest.java
@@ -0,0 +1,9 @@
+package ncr.res.mobilepos.giftcard.test;
+
+import ncr.res.mobilepos.test.TestRunnerScenario;
+
+public class GiftCardTest extends TestRunnerScenario{
+	public GiftCardTest() {
+		super(new GiftCardSteps());
+	}
+}

a8f7c260d9729c9cc0c591dcc4450198c0be0ed3
ts185174 <ts185174@ncr.com>
2017-04-10 02:02:28 +0000

    MEXSVR-176 Reverted to reject failing test-cases for ncr.res.mobilepos/giftcard.
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/3.3.0@66668 9234da4d-b7fe-4b44-acf3-10f21a033f33

--- test/ncr/res/mobilepos/giftcard/test/GiftCardTest.java
+++
@@ -1,9 +0,0 @@
-package ncr.res.mobilepos.giftcard.test;
-
-import ncr.res.mobilepos.test.TestRunnerScenario;
-
-public class GiftCardTest extends TestRunnerScenario{
-	public GiftCardTest() {
-		super(new GiftCardSteps());
-	}
-}

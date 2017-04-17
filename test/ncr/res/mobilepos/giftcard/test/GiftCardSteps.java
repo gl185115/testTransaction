29b2f78bfc970d38b514022e083ae86f2778833c
gz250021 <gz250021@ncr.com>
2017-04-17 12:28:23 +0000

    MEXSVR-176:XMLファイルが存在する場合と存在しない場合、両方ともケースを追加すること。
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/3.3.0@66897 9234da4d-b7fe-4b44-acf3-10f21a033f33

--- test/ncr/res/mobilepos/giftcard/test/GiftCardSteps.java
+++ test/ncr/res/mobilepos/giftcard/test/GiftCardSteps.java
@@ -7,20 +7,29 @@ import javax.servlet.ServletContext;
 
 import org.jbehave.core.annotations.AfterScenario;
 import org.jbehave.core.annotations.BeforeScenario;
+import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 import org.jbehave.core.steps.Steps;
 import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import junit.framework.Assert;
 
 import ncr.res.giftcard.toppan.model.Config;
+import ncr.res.mobilepos.constant.EnvironmentEntries;
+import ncr.res.mobilepos.daofactory.JndiDBManagerMSSqlServer;
 import ncr.res.mobilepos.giftcard.resource.ToppanGiftcardResource;
+import ncr.res.mobilepos.helper.DebugLogger;
+import ncr.res.mobilepos.helper.Logger;
 import ncr.res.mobilepos.helper.Requirements;
+import ncr.res.mobilepos.helper.SnapLogger;
+import ncr.res.mobilepos.helper.SpmFileWriter;
 import ncr.res.mobilepos.helper.XmlSerializer;
 import ncr.res.mobilepos.model.ResultBase;
+import ncr.res.mobilepos.property.SQLStatement;
 
 @RunWith(PowerMockRunner.class)
 @SuppressWarnings("deprecation")
@@ -53,6 +62,28 @@ public class GiftCardSteps extends Steps {
 		}
 	}
 	
+    @Given("toppan-gift.xml file does not exist.")
+    public void giftXmlNotFound() {
+    	Field toppanGiftcardConfigField;
+		try {
+			toppanGiftcardConfigField = toppanGiftcardResource.getClass().getDeclaredField("toppanGiftcardConfig");
+	    	toppanGiftcardConfigField.setAccessible(true);
+	    	toppanGiftcardConfigField.set(toppanGiftcardResource, null);
+		} catch (NoSuchFieldException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
     @PrepareForTest(ToppanGiftcardResource.class)
 	@When("I test QueryMember with storeid $storeid workstationid $workstationid transactionid $transactionid test $test giftcard $giftcard")
 	public final void giftCardQuery(

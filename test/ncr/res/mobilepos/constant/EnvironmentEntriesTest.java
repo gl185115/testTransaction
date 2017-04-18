2211fe985dc12aca8d2bca0466ca4d1d38c51cad
ts185174 <ts185174@ncr.com>
2017-04-18 08:46:48 +0000

    MEXSVR-197 Added new param to EnvironmentalEntriesTest.
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/3.3.0@66911 9234da4d-b7fe-4b44-acf3-10f21a033f33

--- test/ncr/res/mobilepos/constant/EnvironmentEntriesTest.java
+++ test/ncr/res/mobilepos/constant/EnvironmentEntriesTest.java
@@ -1,76 +1,140 @@
 package ncr.res.mobilepos.constant;
 
-import org.junit.Assert;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.springframework.core.env.Environment;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
-import static org.junit.Assert.*;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.fail;
 
 public class EnvironmentEntriesTest {
 
     private static final String KEY_ROOT = "java:comp/env";
+
+    // 1
     private static final String KEY_SERVER_ID = "serverID";
-    private static final String KEY_IOW_PATH = "iowPath";
-    private static final String KEY_TRACE_PATH = "tracePath";
+    // 2
     private static final String KEY_DEBUG_LEVEL = "debugLevel";
-    private static final String KEY_SNAP_PATH = "snapPath";
+    // 3
+    private static final String KEY_PRINT_PAPER_LENGTH = "Printpaperlength";
+    // 4
+    private static final String KEY_CUSTOM_MAINTENANCE_BASE_PATH ="customMaintenanceBasePath";
+    // 5
+    private static final String KEY_CUSTOM_RESOURCE_BASE_PATH = "customResourceBasePath";
+    // 6
+    private static final String KEY_IOW_PATH = "iowPath";
+    // 7
+    private static final String KEY_UI_CONSOLE_LOG_PATH = "UiConsoleLogPath";
+    // 8
+    private static final String KEY_DEVICE_LOG_PATH = "deviceLogPath";
+    // 9
+    private static final String KEY_TRACE_PATH = "tracePath";
+    // 10
     private static final String KEY_SPM_PATH = "Journalization/spmPath";
+    // 11
+    private static final String KEY_SNAP_PATH = "snapPath";
+    // 12
+    private static final String KEY_REPORT_FORMAT_NEW_PATH = "ReportFormatNewPath";
+    // 13
+    private static final String KEY_NR_RCPT_FORMAT_PATH = "nrRcptFormatPath";
+    // 14
+    private static final String KEY_LOCAL_PRINTER = "localPrinter";
+    // 15
+    private static final String KEY_SCHEDULE_FILE_PATH = "scheduleFilePath";
+    // 16
+    private static final String KEY_CUSTOM_PARAM_BASE_PATH = "customParamBasePath";
+    // 17
+
+    @Before
+    public void setUp() throws NamingException {
+        System.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.naming.java.javaURLContextFactory");
+        System.setProperty(Context.URL_PKG_PREFIXES, "org.apache.naming");
+        InitialContext initContext = new InitialContext();
+
+        initContext.createSubcontext("java:comp");
+        initContext.createSubcontext("java:comp/env");
+        initContext.createSubcontext("java:comp/env/Journalization");
+
+        initContext.bind("java:comp/env/serverID", "A010");
+        initContext.bind("java:comp/env/debugLevel", 2);
+        initContext.bind("java:comp/env/Printpaperlength", "2");
+        initContext.bind("java:comp/env/customMaintenanceBasePath", "c:/ncr/res/custom/");
+        initContext.bind("java:comp/env/customResourceBasePath", "c:/ncr/res/custom/");
+
+        initContext.bind("java:comp/env/iowPath", "c:/ncr/res/log");
+        initContext.bind("java:comp/env/UiConsoleLogPath", "c:/ncr/res/log/UiConsoleLog.txt");
+        initContext.bind("java:comp/env/deviceLogPath", "c:/ncr/res/log");
+        initContext.bind("java:comp/env/tracePath", "c:/ncr/res/dbg");
+        initContext.bind("java:comp/env/Journalization/spmPath", "c:/ncr/res/log/SPM_DIR");
+
+        initContext.bind("java:comp/env/snapPath","c:/ncr/res/log/snap_dir");
+        initContext.bind("java:comp/env/ReportFormatNewPath","c:/ncr/res/custom/ReportFormatNew.xml");
+        initContext.bind("java:comp/env/nrRcptFormatPath","c:/ncr/res/custom/NormalReceiptFormat.xml");
+        initContext.bind("java:comp/env/localPrinter","c:/ncr/res/link/systemconfig/interface.xml");
+        initContext.bind("java:comp/env/scheduleFilePath","/schedule.xml");
+
+        initContext.bind("java:comp/env/customParamBasePath", "test/resources/cust/para");
+        initContext.bind("java:comp/env/paraBasePath", "test/resources/para");
+    }
+
+    @After
+    public void tearDown() throws NamingException {
+        InitialContext initContext = new InitialContext();
+        initContext.destroySubcontext("java:comp/env/Journalization");
+        initContext.destroySubcontext("java:comp/env");
+        initContext.destroySubcontext("java:comp");
+    }
 
     @Test
-    public void getInstance() throws Exception {
-        Context mockInitialContext = mock(Context.class);
-        Context mockEnvContext = mock(Context.class);
-        when(mockInitialContext.lookup(anyString())).thenReturn(mockEnvContext);
+    public void getInstance() throws NamingException {
+        try {
+            EnvironmentEntries envInit = EnvironmentEntries.initInstance(new InitialContext());
+            assertNotNull(envInit);
 
-        when(mockEnvContext.lookup(KEY_SERVER_ID)).thenReturn("testServerID");
-        when(mockEnvContext.lookup(KEY_IOW_PATH)).thenReturn("testIowPath");
-        when(mockEnvContext.lookup(KEY_TRACE_PATH)).thenReturn("testTracePath");
-        when(mockEnvContext.lookup(KEY_DEBUG_LEVEL)).thenReturn(0);
-        when(mockEnvContext.lookup(KEY_SNAP_PATH)).thenReturn("testSnapPath");
-        when(mockEnvContext.lookup(KEY_SPM_PATH)).thenReturn("testSpmPath");
+            EnvironmentEntries env = EnvironmentEntries.getInstance();
+            assertEquals(envInit, env);
 
-        EnvironmentEntries envInit = EnvironmentEntries.initInstance(mockInitialContext);
-        assertNotNull(envInit);
-        EnvironmentEntries env = EnvironmentEntries.getInstance();
-        assertEquals(envInit, env);
+            assertEquals("A010", env.getServerId());
+            assertEquals(2, env.getDebugLevel());
+            assertEquals("2", env.getPrintPaperLength());
+            assertEquals("c:/ncr/res/custom/", env.getCustomMaintenanceBasePath());
+            assertEquals("c:/ncr/res/custom/", env.getCustomResourceBasePath());
 
-        assertEquals(env.getServerId(), "testServerID");
-        assertEquals(env.getIowPath(), "testIowPath");
-        assertEquals(env.getTracePath(), "testTracePath");
-        assertEquals(env.getDebugLevel(), 0);
-        assertEquals(env.getSnapPath(), "testSnapPath");
-        assertEquals(env.getSpmPath(), "testSpmPath");
+            assertEquals("c:/ncr/res/log", env.getIowPath());
+            assertEquals("c:/ncr/res/log/UiConsoleLog.txt", env.getUiConsoleLogPath());
+            assertEquals("c:/ncr/res/log", env.getDeviceLogPath());
+            assertEquals("c:/ncr/res/dbg", env.getTracePath());
+            assertEquals("c:/ncr/res/log/SPM_DIR", env.getSpmPath());
+
+            assertEquals("c:/ncr/res/log/snap_dir", env.getSnapPath());
+            assertEquals("c:/ncr/res/custom/ReportFormatNew.xml", env.getReportFormatNewPath());
+            assertEquals("c:/ncr/res/custom/NormalReceiptFormat.xml", env.getNrRcptFormatPath());
+            assertEquals("c:/ncr/res/link/systemconfig/interface.xml", env.getLocalPrinter());
+            assertEquals("/schedule.xml", env.getScheduleFilePath());
+
+            assertEquals("test/resources/cust/para", env.getCustomParamBasePath());
+            assertEquals("test/resources/para", env.getParaBasePath());
+
+        } catch (Exception e) {
+            fail("No Exception exptected.");
+        }
     }
 
     @Test
     public void initInstanceFailed() {
-        // InitInstance gets failed by config error.
-        // Internally when it throws NamingExceptionin, Constructor fails and instance gets null.
-        Context mockInitialContext = mock(Context.class);
-        Context mockEnvContext = mock(Context.class);
         try {
-            when(mockInitialContext.lookup(anyString())).thenReturn(mockEnvContext);
-
-            when(mockEnvContext.lookup(KEY_SERVER_ID)).thenReturn("testServerID");
-            when(mockEnvContext.lookup(KEY_IOW_PATH)).thenReturn("testIowPath");
-            when(mockEnvContext.lookup(KEY_TRACE_PATH)).thenReturn("testTracePath");
-            when(mockEnvContext.lookup(KEY_DEBUG_LEVEL)).thenReturn(0);
-            when(mockEnvContext.lookup(KEY_SNAP_PATH)).thenReturn("testSnapPath");
-            when(mockEnvContext.lookup(KEY_SPM_PATH)).thenReturn("testSpmPath");
-
-            // Throws NamingException while lookup.
-            when(mockEnvContext.lookup(KEY_SERVER_ID)).thenThrow(new NamingException());
-            EnvironmentEntries envInit = EnvironmentEntries.initInstance(mockInitialContext);
+            InitialContext init = new InitialContext();
+            init.unbind("java:comp/env/serverID");
+            EnvironmentEntries.initInstance(new InitialContext());
+            // NamingException has to be thrown.
             fail();
         } catch (NamingException e) {
-
         }
         // Initialization fails then getInstance returns null.
         assertNull(EnvironmentEntries.getInstance());

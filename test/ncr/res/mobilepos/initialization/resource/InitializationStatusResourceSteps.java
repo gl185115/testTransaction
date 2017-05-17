fc516ba774e0cf6386443444b67fe22ec1714456
ts185174 <ts185174@ncr.com>
2017-05-17 03:17:30 +0000

    MEXSVR-180 Replaced business logic factory initialization to mock. Since filling criteria for the factories are not in its test-scope.
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/3.3.0@67990 9234da4d-b7fe-4b44-acf3-10f21a033f33

--- test/ncr/res/mobilepos/initialization/resource/InitializationStatusResourceSteps.java
+++ test/ncr/res/mobilepos/initialization/resource/InitializationStatusResourceSteps.java
@@ -9,15 +9,19 @@ import org.jbehave.core.steps.Steps;
 import org.powermock.api.mockito.PowerMockito;
 
 import java.lang.reflect.Field;
+import java.util.List;
 
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletContext;
 
+import ncr.res.giftcard.toppan.model.Config;
 import ncr.res.mobilepos.barcodeassignment.factory.BarcodeAssignmentFactory;
+import ncr.res.mobilepos.barcodeassignment.model.BarcodeAssignment;
 import ncr.res.mobilepos.constant.EnvironmentEntries;
-import ncr.res.mobilepos.constant.WindowsEnvironmentVariables;
 import ncr.res.mobilepos.daofactory.JndiDBManagerMSSqlServer;
 import ncr.res.mobilepos.giftcard.factory.ToppanGiftCardConfigFactory;
+import ncr.res.mobilepos.helper.DBInitiator;
 import ncr.res.mobilepos.helper.DebugLogger;
 import ncr.res.mobilepos.helper.Logger;
 import ncr.res.mobilepos.helper.Requirements;
@@ -25,10 +29,12 @@ import ncr.res.mobilepos.helper.ResultBaseHelper;
 import ncr.res.mobilepos.helper.SnapLogger;
 import ncr.res.mobilepos.helper.SpmFileWriter;
 import ncr.res.mobilepos.model.ResultBase;
+import ncr.res.mobilepos.pricing.model.QrCodeInfo;
 import ncr.res.mobilepos.promotion.factory.QrCodeInfoFactory;
 import ncr.res.mobilepos.property.SQLStatement;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 
 public class InitializationStatusResourceSteps extends Steps {
     /**
@@ -40,15 +46,11 @@ public class InitializationStatusResourceSteps extends Steps {
      */
     InitializationStatusResource testInitStatusResource;
 
+    private DBInitiator dbInit;
+
     @BeforeScenario
     public final void SetUpClass() throws Exception {
         Requirements.SetUp();
-        InitialContext initContext = new InitialContext();
-        ServletContext servletContext = Requirements.getMockServletContext();
-        EnvironmentEntries env = EnvironmentEntries.getInstance();
-        BarcodeAssignmentFactory.initialize(env.getParaBasePath());
-        ToppanGiftCardConfigFactory.initialize(env.getCustomParamBasePath());
-        QrCodeInfoFactory.initialize(WindowsEnvironmentVariables.getInstance().getSystemPath());
         testInitStatusResource = new InitializationStatusResource();
     }
 
@@ -56,6 +58,35 @@ public class InitializationStatusResourceSteps extends Steps {
     public final void TearDownClass() {
         Requirements.TearDown();
     }
+
+    @Given("Web application was initialized")
+    public void initInstance() throws NamingException {
+        InitialContext initContext = new InitialContext();
+        ServletContext servletContext = Requirements.getMockServletContext();
+        EnvironmentEntries env = EnvironmentEntries.getInstance();
+    }
+
+    @Given("Business logic factories were initialized")
+    public void initBusinessLogicFactories() throws Exception {
+        // Mocks BarcodeAssignment.
+        BarcodeAssignment baMock = mock(BarcodeAssignment.class);
+        Field barcodeAssignmentInstance = BarcodeAssignmentFactory.class.getDeclaredField("instance");
+        barcodeAssignmentInstance.setAccessible(true);
+        barcodeAssignmentInstance.set(null, baMock);
+
+        // Mocks Config for GiftCard.
+        Config giftCardConfigMock = mock(Config.class);
+        Field giftCardConfigInstance = ToppanGiftCardConfigFactory.class.getDeclaredField("instance");
+        giftCardConfigInstance.setAccessible(true);
+        giftCardConfigInstance.set(null, giftCardConfigMock);
+
+        // Mocks BarcodeAssignment.
+        List<QrCodeInfo> qrCodeInfosMock = mock(List.class);
+        Field qrCodeInfosInstance = QrCodeInfoFactory.class.getDeclaredField("instance");
+        qrCodeInfosInstance.setAccessible(true);
+        qrCodeInfosInstance.set(null, qrCodeInfosMock);
+    }
+
 
     @Given("$className failed to instantiate")
     public void initFailureEnvironmentalVariable(final String className) throws IllegalAccessException {

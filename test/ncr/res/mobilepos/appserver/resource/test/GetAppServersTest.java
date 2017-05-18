1c17dc8da037cf33e31ef107b0ef1557981ef0aa
jd185128 <jd185128@ncr.com>
2017-05-18 05:58:05 +0000

    MEXUI-418 - Added appserver test.
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/3.3.0@68056 9234da4d-b7fe-4b44-acf3-10f21a033f33

---
+++ test/ncr/res/mobilepos/appserver/resource/test/GetAppServersTest.java
@@ -0,0 +1,9 @@
+package ncr.res.mobilepos.appserver.resource.test;
+
+import ncr.res.mobilepos.test.TestRunnerScenario;
+
+public class GetAppServersTest extends TestRunnerScenario{
+	public GetAppServersTest() {
+		super(new AppServerSteps());
+	}
+}

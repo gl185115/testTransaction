3175c8e9cfd8be2671729a0abc051acea3f109f4
jz250243 <jz250243@ncr.com>
2017-06-06 04:11:36 +0000

    MEXUI-255 [KINOP] 会員登録 shibingqian
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/3.3.0@68753 9234da4d-b7fe-4b44-acf3-10f21a033f33

--- src/ncr/res/mobilepos/customerSearch/constants/CustomerSearchConstants.java
+++ src/ncr/res/mobilepos/customerSearch/constants/CustomerSearchConstants.java
@@ -10,6 +10,15 @@ public class CustomerSearchConstants {
 
     /** search buying history use url */
     public static final String API_B0407 = "/member/MemberTradeListApi/exec.do";
+    
+    /** get changed login key use url */
+    public static final String API_B0001 = "/login/LoginKeyConvertApi/exec.do";
+    
+    /** get rank info use url */
+    public static final String API_B2001 = "/rank/GetRankDetailApi/exec.do";
+    
+    /** get member info use url */
+    public static final String API_B0102 = "/member/MemberDetailApi/exec.do";
 
     /** the Category column of the table PRM_SYSTEM_CONFIG  */
     public static final String CATEGORY = "MemberServer";
@@ -25,4 +34,16 @@ public class CustomerSearchConstants {
     
     /** Timeout the KeyId column of the table PRM_SYSTEM_CONFIG */
     public static final String KEYID_MEMBERSERVERTIMEOUT = "MemberServerTimeout";
+    
+    /** Dummy Xml - LoginKey */
+    public static final String LOGINKEYXMLPATH = "D:/DummyXml/loginKey_success.xml";
+    
+    /** Dummy Xml - LoginKey */
+    public static final String RANKXMLPATH = "D:/DummyXml/rank_success.xml";
+    
+    /** Dummy Xml - LoginKey */
+    public static final String MEMBERINFOXMLPATH = "D:/DummyXml/memberInfo_success.xml";
+    
+    /** Dummy Xml - Error */
+    public static final String ERRORXMLPATH = "D:/DummyXml/error.xml";
 }

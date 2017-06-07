161d4b3277d1c4962617818b0d63090368935e0c
ks250510 <ks250510@ncr.com>
2017-06-07 08:58:23 +0000

    MEXSVR-234 特売自動値引のテストコードを追加
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/3.3.0@68817 9234da4d-b7fe-4b44-acf3-10f21a033f33

---
+++ test/ncr/res/mobilepos/department/resource/test/DiscountDepartmentResourceTest.java
@@ -0,0 +1,10 @@
+package ncr.res.mobilepos.department.resource.test;
+
+import ncr.res.mobilepos.test.TestRunnerScenario;
+
+public class DiscountDepartmentResourceTest extends TestRunnerScenario{
+    public DiscountDepartmentResourceTest(){
+        super(new DiscountDepartmentResourceSteps());
+    }
+}
+

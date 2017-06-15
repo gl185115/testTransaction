b24b353dc78e19498380d6a380dc905b5dd770df
gz250021 <gz250021@ncr.com>
2017-06-15 03:48:39 +0000

    MEXSVR-269 [KINOP] 顧客検索
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/3.3.0@69189 9234da4d-b7fe-4b44-acf3-10f21a033f33

--- src/ncr/res/mobilepos/customeraccount/dao/SQLServerCustomerInfoDAO.java
+++ src/ncr/res/mobilepos/customeraccount/dao/SQLServerCustomerInfoDAO.java
@@ -13,17 +13,21 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 import ncr.realgate.util.Trace;
 import ncr.res.mobilepos.customeraccount.model.Customer;
+import ncr.res.mobilepos.customeraccount.model.LoyaltyAccount;
+import ncr.res.mobilepos.customeraccount.model.LoyaltyAccountInfo;
 import ncr.res.mobilepos.daofactory.AbstractDao;
 import ncr.res.mobilepos.daofactory.DBManager;
-import ncr.res.mobilepos.daofactory.JndiDBManager;
 import ncr.res.mobilepos.daofactory.JndiDBManagerMSSqlServer;
 import ncr.res.mobilepos.exception.DaoException;
-import ncr.res.mobilepos.exception.SQLStatementException;
 import ncr.res.mobilepos.helper.DebugLogger;
 import ncr.res.mobilepos.helper.Logger;
+import ncr.res.mobilepos.helper.StringUtility;
 import ncr.res.mobilepos.property.SQLStatement;
 
 /**
@@ -46,6 +50,7 @@ extends AbstractDao implements ICustomerDAO {
      * The Trace Printer.
      */
     private Trace.Printer tp;
+	private final SQLStatement sqlStatement;
 
     /**
      * The class constructor.
@@ -56,6 +61,7 @@ extends AbstractDao implements ICustomerDAO {
 
         tp = DebugLogger.getDbgPrinter(
                 Thread.currentThread().getId(), getClass());
+		this.sqlStatement = SQLStatement.getInstance();
     }
     /**
      * Getter.
@@ -147,6 +153,140 @@ extends AbstractDao implements ICustomerDAO {
             tp.methodExit(customerInfo);
         }
 
-        return customerInfo;
-    }
+		return customerInfo;
+	}
+
+	/**
+	 * Gets LoyaltyAccountInfo by specifying the companyId,storeId Fuzzying the
+	 * connName,connKanaName,connTel.
+	 *
+	 * @param companyId
+	 *            The company id to find.
+	 * @param storeId
+	 *            The store id to find.
+	 * @param connName
+	 *            The connName to find.
+	 * @param connKanaName
+	 *            The connKanaName to find.
+	 * @param connTel
+	 *            The connTel to find.
+	 * @return Return the LoyaltyAccountInfo.
+	 * @throws DaoException
+	 *             Exception thrown when searching for LoyaltyAccount fails.
+	 */
+	public LoyaltyAccountInfo getLoyaltyAccountInfo(String companyId, String storeId, String connName,
+			String connKanaName, String connTel) throws DaoException {
+
+		tp.methodEnter("getLoyaltyAccountList");
+		tp.println("companyId", companyId).println("storeId", storeId).println("connName", connName)
+				.println("connKanaName", connKanaName).println("connTel", connTel);
+
+		Connection connection = null;
+		PreparedStatement select = null;
+		ResultSet result = null;
+		LoyaltyAccountInfo loyaltyAccountInfo = new LoyaltyAccountInfo();
+		List<LoyaltyAccount> LoyaltyAccountList = new ArrayList<LoyaltyAccount>();
+
+		try {
+
+			connection = dbManager.getConnection();
+
+			StringBuffer sql = new StringBuffer();
+			sql.append(" ( ");
+			if (!StringUtility.isNullOrEmpty(connName)) {
+				sql.append(" connName LIKE '" + connName + "%' ");
+			}
+			if (!StringUtility.isNullOrEmpty(connKanaName) && !StringUtility.isNullOrEmpty(connName)) {
+				sql.append(" or connKanaName LIKE '" + connKanaName + "%' ");
+			}
+			if (!StringUtility.isNullOrEmpty(connKanaName) && StringUtility.isNullOrEmpty(connName)) {
+				sql.append(" connKanaName LIKE '" + connKanaName + "%' ");
+			}
+			if (!StringUtility.isNullOrEmpty(connTel)
+					&& (!StringUtility.isNullOrEmpty(connName) || !StringUtility.isNullOrEmpty(connKanaName))) {
+				sql.append(" or connTel LIKE '" + connTel + "%' ");
+			}
+			if (!StringUtility.isNullOrEmpty(connTel) && StringUtility.isNullOrEmpty(connName)
+					&& StringUtility.isNullOrEmpty(connKanaName)) {
+				sql.append(" connTel LIKE '" + connTel + "%' ");
+			}
+			sql.append(" ) ");
+
+			select = prepareStatementCountUnsendPoslog(connection, companyId, storeId, sql.toString());
+			select.setString(1, companyId);
+			select.setString(2, storeId);
+			result = select.executeQuery();
+			while (result.next()) {
+				LoyaltyAccount tempLoyaltyAccount = new LoyaltyAccount();
+				tempLoyaltyAccount.setConnCode(result.getString(result.findColumn("ConnCode")));
+				tempLoyaltyAccount.setConnName(result.getString(result.findColumn("ConnName")));
+				tempLoyaltyAccount.setConnKanaName(result.getString(result.findColumn("ConnKanaName")));
+				tempLoyaltyAccount.setConnTel(result.getString(result.findColumn("ConnTel")));
+
+				tempLoyaltyAccount.setCompanyId(result.getString(result.findColumn("CompanyId")));
+				tempLoyaltyAccount.setStoreId(result.getString(result.findColumn("StoreId")));
+				tempLoyaltyAccount.setOrgCode(result.getString(result.findColumn("OrgCode")));
+				tempLoyaltyAccount.setConnCat(result.getInt(result.findColumn("ConnCat")));
+				tempLoyaltyAccount.setConnGrp(result.getString(result.findColumn("ConnGrp")));
+				tempLoyaltyAccount.setConnZip(result.getString(result.findColumn("ConnZip")));
+				tempLoyaltyAccount.setConnAddr(result.getString(result.findColumn("ConnAddr")));
+				tempLoyaltyAccount.setConnFax(result.getString(result.findColumn("ConnFax")));
+				tempLoyaltyAccount.setConnOwner(result.getString(result.findColumn("ConnOwner")));
+				tempLoyaltyAccount.setConnLimit(result.getString(result.findColumn("ConnLimit")));
+				tempLoyaltyAccount.setConnCloseDate(result.getString(result.findColumn("ConnCloseDate")));
+				tempLoyaltyAccount.setOnlineType(result.getInt(result.findColumn("OnlineType")));
+				tempLoyaltyAccount.setSlipType(result.getInt(result.findColumn("SlipType")));
+				tempLoyaltyAccount.setPaymentType(result.getInt(result.findColumn("PaymentType")));
+				tempLoyaltyAccount.setCostRate(result.getInt(result.findColumn("CostRate")));
+				tempLoyaltyAccount.setConnStartDate(result.getString(result.findColumn("ConnStartDate")));
+				tempLoyaltyAccount.setConnEndDate(result.getString(result.findColumn("ConnEndDate")));
+				tempLoyaltyAccount.setConnSubCode(result.getString(result.findColumn("ConnSubCode")));
+				tempLoyaltyAccount.setSubCode1(result.getString(result.findColumn("SubCode1")));
+				tempLoyaltyAccount.setSubCode2(result.getString(result.findColumn("SubCode2")));
+				tempLoyaltyAccount.setSubCode3(result.getString(result.findColumn("SubCode3")));
+				tempLoyaltyAccount.setSubCode4(result.getString(result.findColumn("SubCode4")));
+				tempLoyaltyAccount.setSubCode5(result.getString(result.findColumn("SubCode5")));
+				tempLoyaltyAccount.setSubNum1(result.getDouble(result.findColumn("SubNum1")));
+				tempLoyaltyAccount.setSubNum2(result.getDouble(result.findColumn("SubNum2")));
+				tempLoyaltyAccount.setSubNum3(result.getDouble(result.findColumn("SubNum3")));
+				tempLoyaltyAccount.setSubNum4(result.getDouble(result.findColumn("SubNum4")));
+				tempLoyaltyAccount.setSubNum5(result.getDouble(result.findColumn("SubNum5")));
+
+				LoyaltyAccountList.add(tempLoyaltyAccount);
+
+			}
+			if (LoyaltyAccountList == null || LoyaltyAccountList.size() == 0) {
+				tp.println("LoyaltyAccountInfo not found.");
+			}
+			loyaltyAccountInfo.setLoyaltyAccountList(LoyaltyAccountList);
+
+		} catch (SQLException e) {
+
+			LOGGER.logAlert("CustAcnt", "SQLServerCustomerInfo.getLoyaltyAccountList", Logger.RES_EXCEP_SQL,
+					"Failed getting customer information " + e.getMessage());
+			throw new DaoException("SQLException:@SQLServerCustomerInfo.getLoyaltyAccountList ", e);
+
+		} finally {
+			closeConnectionObjects(connection, select, result);
+
+			tp.methodExit(loyaltyAccountInfo);
+		}
+		return loyaltyAccountInfo;
+	}
+
+	/**
+	 * Populates PreparedStatement with given SQL.
+	 * 
+	 * @param connection
+	 *            DB-connection
+	 * @param checkSql
+	 * @return
+	 */
+	private PreparedStatement prepareStatementCountUnsendPoslog(Connection connection, String companyId, String storeId,
+			String sql) throws SQLException {
+		// Replace %s to check sql.
+		String query = String.format(sqlStatement.getProperty("get-loyalty-account"), sql);
+		PreparedStatement statement = connection.prepareStatement(query);
+		return statement;
+	}
 }
---
+++ test/ncr/res/mobilepos/customeraccount/test/CustomerAccountTest.java
@@ -0,0 +1,9 @@
+package ncr.res.mobilepos.customeraccount.test;
+
+import ncr.res.mobilepos.test.TestRunnerScenario;
+
+public class CustomerAccountTest extends TestRunnerScenario {
+	public CustomerAccountTest() {
+		super(new CustomerAccountSteps());
+	}
+}

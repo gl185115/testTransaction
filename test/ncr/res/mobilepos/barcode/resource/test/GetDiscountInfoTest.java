903eb82df068e5ffd71d022e3c8e0563183e195f
jd185128 <jd185128@ncr.com>
2017-07-05 06:15:47 +0000

    MEXUI-504 -[KINOP][Common Store/Terminal] Transaction Discount
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/3.3.0@70220 9234da4d-b7fe-4b44-acf3-10f21a033f33

---
+++ test/ncr/res/mobilepos/barcode/resource/test/GetDiscountInfoTest.java
@@ -0,0 +1,9 @@
+package ncr.res.mobilepos.barcode.resource.test;
+
+import ncr.res.mobilepos.test.TestRunnerScenario;
+
+public class GetDiscountInfoTest extends TestRunnerScenario{
+	public GetDiscountInfoTest() {
+		super(new GetDiscountInfoSteps());
+	}
+}

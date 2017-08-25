2a84893fd3d0c72846d5a2384d9b569f3ec6b1df
gz250021 <gz250021@ncr.com>
2017-08-25 02:26:46 +0000

    MEXSVR-166　無効なソースを削除する。
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/3.3.0@72110 9234da4d-b7fe-4b44-acf3-10f21a033f33

--- src/ncr/res/mobilepos/promotion/resource/PromotionResource.java
+++ src/ncr/res/mobilepos/promotion/resource/PromotionResource.java
@@ -1,8 +1,6 @@
 package ncr.res.mobilepos.promotion.resource;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;

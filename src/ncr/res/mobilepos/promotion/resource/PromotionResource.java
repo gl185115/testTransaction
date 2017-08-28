6bc17a2d8a729dd455118ef7254f3fe23e895821
gz250021 <gz250021@ncr.com>
2017-08-28 14:16:46 +0000

    MEXSVR-318 [KINOP][BUG][結合] 金種登録　電子マネー利用可否フラグを正しく判定できていない
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/3.3.0@72170 9234da4d-b7fe-4b44-acf3-10f21a033f33

--- src/ncr/res/mobilepos/promotion/resource/PromotionResource.java
+++ src/ncr/res/mobilepos/promotion/resource/PromotionResource.java
@@ -1421,12 +1421,7 @@ public class PromotionResource {
 		item.setMdKanaName(json.getString("mdKanaName"));
 		item.setSalesPrice2(json.getDouble("salesPrice2"));
 		item.setPaymentType(json.getInt("paymentType"));
-		//item.setPublishingCode(json.getString("venderCode"));
-		//if (json.getString("posMdType") == null) {
-		//	item.setNonSales(json.getInt("exceptionFlag"));
-		//} else {
-		//	item.setNonSales(json.getInt("posMdType"));
-		//}
+		item.setPublishingCode(json.getString("publishingCode"));
 		item.setNonSales(json.getInt("nonSales"));
 		item.setSubCode1(json.getString("subCode1"));
 		item.setSubCode2(json.getString("subCode2"));

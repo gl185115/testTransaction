e077c979c8c4e0bf57da1c6d9c026d95d76ce6e2
gz250021 <gz250021@ncr.com>
2018-02-23 09:01:25 +0000

    MEXSVR-364： 　「update-line」SQLに関する下記サービスを削除すること。 　/rest/line/delete 　/rest/line/create 　/rest/line/maintenance 　/rest/line/detail
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/branches/3.3.2@76014 9234da4d-b7fe-4b44-acf3-10f21a033f33

--- src/ncr/res/mobilepos/line/dao/ILineDAO.java
+++ src/ncr/res/mobilepos/line/dao/ILineDAO.java
@@ -41,49 +41,5 @@ public interface ILineDAO {
      *  The Exception thrown when getting the List of Lines failed.
      */
     List<Line> listLines(String retailstoreid, String department, String key, String name, int limit) throws DaoException;  
-        
-    /**
-     * Deletes a Line.
-     * @param retailStoreID The retailstoreid of a line.
-     * @param department  The line department.
-     * @param line        The line id.
-     * @return resultBase   The ResultBase object containing
-     *                      resultcode: 0 if success otherwise failed.
-     * @throws DaoException The Exception thrown if error exists.
-     */
-    ResultBase deleteLine(String retailStoreID, String department,
-            String line) throws DaoException;
-    
-    /**
-     * Create a Line.
-     * @param line          The new values of the line.
-     * @return              The ResultBase object containing
-     *                      resultcode: 0 if success otherwise failed.
-     * @throws DaoException The Exception thrown when if error exists.
-     */
-    ResultBase createLine(Line line) throws DaoException;
-    
-    /**
-     * Select Line.
-     * @param retailStoreID - store number
-     * @param department - department number 
-     * @param line - line
-     * @return Line Model
-     * @throws DaoException - exception
-     */
-     ViewLine selectLineDetail(String retailStoreID,
-         String department, String line) throws DaoException;
-     
-     /**
-      * Updates a Line infod
-      *  
-      * @param retailStoreId the store of the to be updated line  info
-      * @param department	the department of the to be updated line info
-      * @param lineid		the lineid of the to be updated line info
-      * @param line			the line object of the to be updated
-      * @return ViewLine object with the updated line
-      * @throws DaoException
-      */
-     ViewLine updateLine (String retailStoreId, String department, String lineid, Line line) throws DaoException;
-     
+
 }

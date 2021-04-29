056c52d1ea0b409150d1a7ec68391d68710614ca
rr185262 <rr185262@ncr.com>
2021-04-29 06:59:31 +0000

    RESD-7445 - [MeX FDMM] Items will no longer bundle as MM/PM after applying a price change  - Fix issue in MM Promotion when pm item has price override  - committed in behalf of Jessel De la Cerna
    git-svn-id: http://svnsin.ncr.com/svn/repos/pa/res/RES%20MeX/Base/resTransaction/trunk@96102 9234da4d-b7fe-4b44-acf3-10f21a033f33

--- src/ncr/res/mobilepos/promotion/helper/TerminalItem.java
+++ src/ncr/res/mobilepos/promotion/helper/TerminalItem.java
@@ -1205,7 +1205,9 @@ public class TerminalItem {
             } else {
                 sumCount = sumCount + mixMatchDetailInfo3.getQuantity();
             }
-            if (rule.getConditionCount3() > 0 && (sumCount / rule.getConditionCount3()) >= 1 && vLastDecisionPrice == 0) {
+            double totalTruePrice = ruleList.stream().mapToDouble((f) -> f.getTruePrice()).sum() + mixMatchDetailInfo3.getTruePrice();
+            if (rule.getConditionCount3() > 0 && (sumCount / rule.getConditionCount3()) >= 1 && vLastDecisionPrice == 0
+            		&& totalTruePrice > rule.getConditionPrice3()) {
                 if (map.containsKey("rule1")) {
                     map.remove("rule1");
                 }
@@ -1306,7 +1308,9 @@ public class TerminalItem {
             } else {
                 sumCount = sumCount + mixMatchDetailInfo2.getQuantity();
             }
-            if (rule.getConditionCount2() > 0 && (sumCount / rule.getConditionCount2()) >= 1 && (vLastDecisionPrice == 0 || vLastDecisionPrice > rule.getAveragePrice2())) {
+            double totalTruePrice = ruleList.stream().mapToDouble((f) -> f.getTruePrice()).sum() + mixMatchDetailInfo2.getTruePrice();
+            if (rule.getConditionCount2() > 0 && (sumCount / rule.getConditionCount2()) >= 1 && (vLastDecisionPrice == 0 || vLastDecisionPrice > rule.getAveragePrice2())
+            		&& totalTruePrice > rule.getConditionPrice2()) {
                 if (map.containsKey("rule1")) {
                     map.remove("rule1");
                 }
@@ -1400,7 +1404,9 @@ public class TerminalItem {
             } else {
                 sumCount = sumCount + mixMatchDetailInfo1.getQuantity();
             }
-            if (rule.getConditionCount1() > 0 &&(sumCount / rule.getConditionCount1()) >= 1 && (vLastDecisionPrice == 0 || vLastDecisionPrice > rule.getAveragePrice1())) {
+            double totalTruePrice = ruleList.stream().mapToDouble((f) -> f.getTruePrice()).sum() + mixMatchDetailInfo1.getTruePrice();
+            if (rule.getConditionCount1() > 0 &&(sumCount / rule.getConditionCount1()) >= 1 && (vLastDecisionPrice == 0 || vLastDecisionPrice > rule.getAveragePrice1())
+            		&& totalTruePrice > rule.getConditionPrice1()) {
                 MixMatchDetailInfo ruleentryinfo1 = new MixMatchDetailInfo();
                 ruleentryinfo1.setEntryId(mixMatchDetailInfo1.getEntryId());
                 ruleentryinfo1.setTruePrice(mixMatchDetailInfo1.getTruePrice());

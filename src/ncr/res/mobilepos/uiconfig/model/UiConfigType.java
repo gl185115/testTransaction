36bca39a52d1a3a698e0a8a9bfd1cc19f2e9157b
Choudhury Amarendra Pramanik <ChoudhuryAmarendra.Pramanik@ncr.com>
2021-12-03 14:33:17 +0530

    RESD-7817- Move SCO AImessage to MeX ResTablet mapping file to custom folder in MeX Host

--- src/ncr/res/mobilepos/uiconfig/model/UiConfigType.java
+++ src/ncr/res/mobilepos/uiconfig/model/UiConfigType.java
@@ -1,3 +1,11 @@
+/****************************************************
+ * Copyright 2021 NCR Corporation
+ *
+ * Date             Quick look Id                  Change
+ * -----------------------------------------------------------
+ * 12-Dec-2021       cp185211            RESD-7817- Move SCO AImessage to MeX ResTablet mapping file to custom folder in MeX Host
+ * 
+ ****************************************************/
 package ncr.res.mobilepos.uiconfig.model;
 
 public enum UiConfigType {
@@ -10,6 +18,7 @@ public enum UiConfigType {
     ADVERTISES("advertise"),
     RECEIPT("receipt"),
     SOUNDS("sounds"),
+    SCOMAPS("scomaps"),
     UNKNOWN("");
 
     private final String key;
@@ -47,6 +56,7 @@ public enum UiConfigType {
     private static final String API_PICKLIST = "picklist";
     private static final String API_ADVERTISES = "advertise";
     private static final String API_RECEIPT = "receipt";
+    private static final String API_SCOMAPS = "scomaps";
 
     /**
      * @param typeParam
@@ -72,6 +82,9 @@ public enum UiConfigType {
                 break;
             case API_RECEIPT:
                 configType = UiConfigType.RECEIPT;
+                break;
+            case API_SCOMAPS:
+                configType = UiConfigType.SCOMAPS;
                 break;
             default:
                 configType = UiConfigType.UNKNOWN;
